507ada6b46cb8a356a2d7b918ce27ea50efb0dbe
SIKSHAA <sikshapriyadarshini1@gmail.com>
2026-06-14 17:34:48 +0530

    Refactor hello.py for improved user input
    Optimized name input handling by stripping whitespace and capitalizing.

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
+#my very first python code prompting to write hello to users.
 n= input("please enter your name")
 print("hello",n)
+
+#optimising the function logic of the above code.
+name=input("Please enter your name:").strip().title()
+print("hello",name)
